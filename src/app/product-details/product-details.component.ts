d89e149ca17a08f278d5bf63f25305bf40b0bff1
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-07-11 17:55:37 +0200

    routing

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  productTitle: string = "";
+
+  constructor(private _route: ActivatedRoute){}
+
+  ngOnInit(): void {
+      this._route.params.subscribe(params => {
+        this.productTitle = params['productId'];
+      });
+  };
 
 }
