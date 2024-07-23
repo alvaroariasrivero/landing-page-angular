1d67353bbcea09033b4b15ff0ab5995bad77cf0e
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-07-23 17:16:19 +0200

    rendering productList

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-import { productsList } from './products.mock';
+import { Component, OnInit } from '@angular/core';
+import { iProduct } from '../models/product.model';
+import { GetProductsService } from '../services/get-products.service';
+// import { productsList } from './products.mock'; //Lista falseada de productos
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
-  productsList = productsList;
+export class ProductsComponent implements OnInit {
+  // productsList = productsList;
+
+  productsList: iProduct[] = [];
+
+  constructor(private _apiService: GetProductsService){}
+
+  ngOnInit(): void {
+      this._apiService.getProducts().subscribe(data => {
+        console.log(data);
+        
+        this.productsList = data
+      })
+  }
 }
